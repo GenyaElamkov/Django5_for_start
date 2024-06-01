c7729d2d81aadc51831bc74480382f8e0ece2b23
GenyaElamkov <genyaelamkov@gmail.com>
2024-06-01 14:41:03 +0300

    lesson 2.4

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,15 @@
-from django.urls import path
-from .views import homepageview
+from django.urls import path, re_path
+from blog import views
 
 
-urlpatterns = [path('', homepageview, name='home'),]
+urlpatterns = [
+    path("", views.index),
+    re_path(r"^about/", views.about, kwargs={'name': "Женя", 'age': 39}),
+    # re_path(r"^about/", views.about),
+    # re_path(r"^about/contact", views.contact),
+    # path("about/", views.about),
+    re_path(r"^contact/", views.contact),
+    path('user/<str:name>/<int:age>', views.user),
+    path('user/<str:name>/', views.user),
+    path('user/', views.user),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
 
+def index(request):
+    return HttpResponse("<h2>Главная</h2>")
 
-def homepageview(request):
-    return HttpResponse("Hello world!")
+
+def about(request, name, age):
+    return HttpResponse(f"""
+        <h2>О пользователе</h2>
+        <p>Имя: {name}</p>
+        <p>Возраст: {age}</p>
+""")
+
+
+def contact(request):
+    return HttpResponse("<h2>Контакты</h2>")
+
+
+# def user(request, name='UserNone', age=0):
+#     return HttpResponse(f"<h2>Имя: {name}. Возраст: {age}</h2>")
+
+
+def user(request, name, age):
+    return HttpResponse(f"<h2>Имя: {name}. Возраст: {age}</h2>")
